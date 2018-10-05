6c07402edc419397f9c620365a6d1b972a2b8828
Saunby <M.Saunby@exeter.ac.uk>
2018-10-05 23:17:47 +0100

    outline

---
+++ sqlserver-example/src/main/java/uk/ac/ex/student/fred/SqlDemo.java
@@ -0,0 +1,9 @@
+package uk.ac.ex.student.fred;
+
+public class SqlDemo {
+
+    public static void main(String[] args) {
+
+     
+     };
+}
---
+++ sqlserver-example/src/test/java/uk/ac/ex/student/fred/SqlDemoTest.java
@@ -0,0 +1,22 @@
+package uk.ac.ex.student.fred;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
+
+import uk.ac.ex.student.fred.SqlPotholeReport;
+
+public class SqlDemoTest {
+
+
+  @Before
+	public void init() {
+      
+	}
+ 
+	@Test
+	public void countTest() {
+    
+	}
+  }
